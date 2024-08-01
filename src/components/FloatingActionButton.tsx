5149763be31054b42673de53c135c1d1ddece5c7
Dimas Febriyanto <dimassfeb@gmail.com>
2024-08-01 16:19:02 +0700

    fixed hidden scroll

--- src/components/FloatingActionButton.tsx
+++ src/components/FloatingActionButton.tsx
@@ -8,7 +8,7 @@ const FloatingActionButton: React.FC<FloatingActionButtonProps> = ({ onClick }) 
     return (
         <button
             onClick={onClick}
-            className="lg:hidden fixed bottom-6 h-16 w-16 right-6 bg-blue-700 text-white rounded-full shadow-lg hover:bg-blue-800 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            className="xl:hidden fixed bottom-6 h-16 w-16 right-6 bg-blue-700 text-white rounded-full shadow-lg hover:bg-blue-800 focus:outline-none focus:ring-2 focus:ring-blue-500"
         >
             <span className="text-md">↑</span> {/* You can use any icon or text here */}
         </button >
