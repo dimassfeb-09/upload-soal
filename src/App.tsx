c17710c008f69a63e075ffce736442956c1494a4
Dimas Febriyanto <dimassfeb@gmail.com>
2024-08-02 11:51:49 +0700

    edit header

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,47 @@
-import React, { useState, useEffect } from 'react';
-import './output.css';
-import './input.css';
-import Question from './components/Question';
-import { toast } from 'react-toastify';
-import { Analytics } from "@vercel/analytics/react"
-import RadioAnswer from './components/RadioAnswer';
-import TableAnswer from './components/TableAnswer';
-import supabase from './utils/supabase';
-import 'react-toastify/dist/ReactToastify.css';
-import MatkulSelect from './components/MatkulSelect';
-import { AnswerData } from './types/AnswerData';
-import FloatingActionButton from './components/FloatingActionButton';
+import React, { useState, useEffect } from "react";
+import "./output.css";
+import "./input.css";
+import Question from "./components/Question";
+import { toast } from "react-toastify";
+import { Analytics } from "@vercel/analytics/react";
+import RadioAnswer from "./components/RadioAnswer";
+import TableAnswer from "./components/TableAnswer";
+import supabase from "./utils/supabase";
+import "react-toastify/dist/ReactToastify.css";
+import MatkulSelect from "./components/MatkulSelect";
+import { AnswerData } from "./types/AnswerData";
+import FloatingActionButton from "./components/FloatingActionButton";
 
 function App() {
-  const [question, setQuestion] = useState<string>('');
+  const [question, setQuestion] = useState<string>("");
   const [data, setData] = useState<AnswerData[]>([]);
   const [alertMessage, setAlertMessage] = useState<string>("");
-  const [selectedAnswer, setSelectedAnswer] = useState<string>('');
+  const [selectedAnswer, setSelectedAnswer] = useState<string>("");
   const [selectedMatkul, setSelectedMatkul] = useState<number>(() => {
-    const savedMatkul = localStorage.getItem('selectedMatkul');
+    const savedMatkul = localStorage.getItem("selectedMatkul");
     return savedMatkul ? parseInt(savedMatkul, 10) : 0;
   });
-  const [source, setSource] = useState<string>('');
-  const [selectedMatkulName, setSelectedMatkulName] = useState<string>('');
+  const [source, setSource] = useState<string>("");
+  const [selectedMatkulName, setSelectedMatkulName] = useState<string>("");
 
   const handleQuestionChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setQuestion(e.target.value);
   };
 
-  const handleSelectedAnswerChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleSelectedAnswerChange = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
     setSelectedAnswer(e.target.value);
   };
 
-  const handleSelectedMatkulChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+  const handleSelectedMatkulChange = (
+    e: React.ChangeEvent<HTMLSelectElement>
+  ) => {
     const selectedId = parseInt(e.target.value, 10);
     setSelectedMatkul(selectedId);
     setSelectedMatkulName(e.target.options[e.target.selectedIndex].text);
     setData([]);
-    localStorage.setItem('selectedMatkul', selectedId.toString());
+    localStorage.setItem("selectedMatkul", selectedId.toString());
   };
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
@@ -53,7 +57,7 @@ function App() {
       resetForm();
       toast.success("Berhasil tambah soal baru!");
     } catch (error) {
-      console.error('Error inserting data:', error);
+      console.error("Error inserting data:", error);
     } finally {
       await fetchData();
       scrollToTop();
@@ -63,43 +67,47 @@ function App() {
   const fetchData = async () => {
     try {
       const { data: fetchedData, error } = await supabase
-        .from('soal')
-        .select('*')
-        .eq('matkul_id', selectedMatkul)
-        .order('created_at', { ascending: false });
+        .from("soal")
+        .select("*")
+        .eq("matkul_id", selectedMatkul)
+        .order("created_at", { ascending: false });
 
       if (error) throw error;
       setData(fetchedData || []);
     } catch (error) {
-      toast.error('Failed to fetch data');
+      toast.error("Failed to fetch data");
     }
   };
 
-  const insertNewData = async (question: string, answer: string, matkul_id: number) => {
+  const insertNewData = async (
+    question: string,
+    answer: string,
+    matkul_id: number
+  ) => {
     try {
       const { error } = await supabase
-        .from('soal')
+        .from("soal")
         .insert([{ question, answer, matkul_id, source }]);
       if (error) throw error;
       sendMessagesNewSoal();
     } catch (error) {
-      console.error('Error inserting new data:', error);
+      console.error("Error inserting new data:", error);
       throw error;
     }
   };
 
   const formatText = (text: string) => {
-    return text.replace(/\n/g, '<br>');
+    return text.replace(/\n/g, "<br>");
   };
 
   const sendMessagesNewSoal = () => {
     const channelB = supabase.channel(`room-${selectedMatkul}`);
     channelB.subscribe((status) => {
-      if (status === 'SUBSCRIBED') {
+      if (status === "SUBSCRIBED") {
         channelB.send({
-          type: 'broadcast',
-          event: 'new-soal',
-          payload: { message: 'Ada soal baru nih!' },
+          type: "broadcast",
+          event: "new-soal",
+          payload: { message: "Ada soal baru nih!" },
         });
       }
     });
@@ -110,13 +118,15 @@ function App() {
   };
 
   const scrollToTop = () => {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+    window.scrollTo({ top: 0, behavior: "smooth" });
   };
 
   const handleRequiredField = () => {
     if (question.trim() === "") setAlertMessage("Soal tidak boleh kosong");
-    if (selectedAnswer.trim() === "") setAlertMessage("Pilih salah satu jawaban yang benar");
-    if (selectedMatkul === 0) setAlertMessage("Pilih mata kuliah terlebih dahulu");
+    if (selectedAnswer.trim() === "")
+      setAlertMessage("Pilih salah satu jawaban yang benar");
+    if (selectedMatkul === 0)
+      setAlertMessage("Pilih mata kuliah terlebih dahulu");
 
     if (alertMessage !== "") {
       toast.error(alertMessage);
@@ -126,9 +136,9 @@ function App() {
   };
 
   const resetForm = () => {
-    setQuestion('');
-    setSelectedAnswer('');
-    setSource('');
+    setQuestion("");
+    setSelectedAnswer("");
+    setSource("");
   };
 
   useEffect(() => {
@@ -136,74 +146,86 @@ function App() {
   }, [selectedMatkul]);
 
   return (
-    <div className="min-h-screen  bg-dark-gray w-full p-5 sm:p-10 lg:p-5 flex flex-col">
-      <h1 className='text-3xl font-bold text-white mb-5'>
-        DARI PADA CEK GAMBAR SATU-SATU MENDING COPY PASTE SINI SOAL-NYA
-      </h1>
+    <>
+      <header className="w-full bg-dark-gray p-3">
+        <h1 className="text-3xl font-bold text-white">
+          DARI PADA CEK GAMBAR SATU-SATU MENDING COPY PASTE SINI SOAL-NYA
+        </h1>
 
-      <p className='text-orange-500 font-bold mb-5'>
-        Download Extention Absolute Enable Right Click & Copy dulu{' '}
-        <a className='text-blue-500 underline' target='_blank' rel='noopener noreferrer' href="https://chromewebstore.google.com/detail/absolute-enable-right-cli/jdocbkpgdakpekjlhemmfcncgdjeiika?hl=en">
-          di sini
-        </a>
-      </p>
-
-      <div className='xl:flex lg:gap-5 h-full'>
-        <form className='w-full xl:w-1/2 flex flex-col gap-5' onSubmit={handleSubmit}>
-          <div className='p-5 border rounded-md'>
-            <MatkulSelect
-              selectedOption={selectedMatkul}
-              setSelectedMatkulName={setSelectedMatkulName}
-              onOptionChange={handleSelectedMatkulChange}
-            />
-          </div>
-          <div className='border rounded-md p-5'>
-            <h1 className='text-4xl text-white font-bold mb-5'>INPUT SOAL</h1>
-            <div className='mb-6'>
-              <label htmlFor="source" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                Sumber / Pengirim <span className='text-red-500 font-bold'>(OPSIONAL)</span>
-              </label>
-              <input
-                type="text"
-                id="source"
-                value={source}
-                className="bg-gray-50 border text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="Masukkan sumber, sertakan link jika ada"
-                onChange={(e) => setSource(e.target.value)}
+        <p className="text-orange-500 font-bold">
+          Download Extention Absolute Enable Right Click & Copy dulu{" "}
+          <a
+            className="text-blue-500 underline"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://chromewebstore.google.com/detail/absolute-enable-right-cli/jdocbkpgdakpekjlhemmfcncgdjeiika?hl=en"
+          >
+            di sini
+          </a>
+        </p>
+      </header>
+      <div className="min-h-screen  bg-dark-gray w-full p-5 sm:p-10 lg:p-5 flex flex-col">
+        <div className="xl:flex lg:gap-5 h-full">
+          <form
+            className="w-full xl:w-1/2 flex flex-col gap-5"
+            onSubmit={handleSubmit}
+          >
+            <div className="p-5 border rounded-md">
+              <MatkulSelect
+                selectedOption={selectedMatkul}
+                setSelectedMatkulName={setSelectedMatkulName}
+                onOptionChange={handleSelectedMatkulChange}
               />
             </div>
+            <div className="border rounded-md p-5">
+              <h1 className="text-4xl text-white font-bold mb-5">INPUT SOAL</h1>
+              <div className="mb-6">
+                <label
+                  htmlFor="source"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Sumber / Pengirim{" "}
+                  <span className="text-red-500 font-bold">(OPSIONAL)</span>
+                </label>
+                <input
+                  type="text"
+                  id="source"
+                  value={source}
+                  className="bg-gray-50 border text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Masukkan sumber, sertakan link jika ada"
+                  onChange={(e) => setSource(e.target.value)}
+                />
+              </div>
 
-            <Question
-              text={question}
-              onTextChange={handleQuestionChange}
+              <Question text={question} onTextChange={handleQuestionChange} />
+              <RadioAnswer
+                selectedOption={selectedAnswer}
+                onOptionChange={handleSelectedAnswerChange}
+              />
+              <button
+                type="submit"
+                className="inline-flex items-center justify-center w-full py-2.5 mt-5 text-lg font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800"
+              >
+                Kirim
+              </button>
+            </div>
+          </form>
+
+          <div className="w-full xl:w-1/2 flex-1">
+            <TableAnswer
+              data={data}
+              setData={setData}
+              matkul_id={selectedMatkul}
+              matkul_name={selectedMatkulName}
             />
-            <RadioAnswer
-              selectedOption={selectedAnswer}
-              onOptionChange={handleSelectedAnswerChange}
-            />
-            <button
-              type="submit"
-              className="inline-flex items-center justify-center w-full py-2.5 mt-5 text-lg font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800"
-            >
-              Kirim
-            </button>
           </div>
-        </form>
-
-        <div className='w-full xl:w-1/2 flex-1'>
-          <TableAnswer
-            data={data}
-            setData={setData}
-            matkul_id={selectedMatkul}
-            matkul_name={selectedMatkulName}
-          />
         </div>
+
+        <FloatingActionButton onClick={scrollToTop} />
+
+        <Analytics />
       </div>
-
-      <FloatingActionButton onClick={scrollToTop} />
-
-      <Analytics/>
-    </div>
+    </>
   );
 }
 
