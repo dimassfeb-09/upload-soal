cbdacf6cda71aeb91751a0fa4ad894342f90113d
Dimas Febriyanto <dimassfeb@gmail.com>
2024-08-02 13:43:14 +0700

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -58,7 +58,6 @@ function App() {
       toast.success("Berhasil tambah soal baru!");
     } catch (error) {
       console.error("Error inserting data:", error);
-      setIsLoading(true);
     } finally {
       setIsLoading(false);
       await fetchData();
@@ -215,12 +214,9 @@ function App() {
               />
               <button
                 type="submit"
-                disabled={isLoading}
-                className={`inline-flex items-center justify-center w-full py-2.5 mt-5 text-lg font-medium text-center text-white ${
-                  isLoading ? "bg-gray-500" : "bg-blue-700 hover:bg-blue-800"
-                } rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900`}
+                className="inline-flex items-center justify-center w-full py-2.5 mt-5 text-lg font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800"
               >
-                {isLoading ? "Loading...." : "Kirim"}
+                Kirim
               </button>
             </div>
           </form>
