ab6931ada5e15648a37ca1c01b5e35a3c1946e2d
Dimas Febriyanto <dimassfeb@gmail.com>
2024-08-02 13:45:16 +0700

    fixed loading

--- src/App.tsx
+++ src/App.tsx
@@ -214,9 +214,14 @@ function App() {
               />
               <button
                 type="submit"
-                className="inline-flex items-center justify-center w-full py-2.5 mt-5 text-lg font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800"
+                disabled={isLoading}
+                className={`inline-flex items-center justify-center w-full py-2.5 mt-5 text-lg font-medium text-center text-white ${
+                  isLoading
+                    ? "bg-gray-500 opacity-70"
+                    : "bg-blue-700 hover:bg-blue-800"
+                } rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900`}
               >
-                Kirim
+                {isLoading ? "Loading...." : "Kirim"}
               </button>
             </div>
           </form>
