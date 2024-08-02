656f4987866c7e2f056771a2c4d019c9413fd963
Dimas Febriyanto <dimassfeb@gmail.com>
2024-08-02 13:41:00 +0700

    update loading

--- src/App.tsx
+++ src/App.tsx
@@ -22,6 +22,7 @@ function App() {
   });
   const [source, setSource] = useState<string>("");
   const [selectedMatkulName, setSelectedMatkulName] = useState<string>("");
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const handleQuestionChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setQuestion(e.target.value);
@@ -45,6 +46,7 @@ function App() {
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    setIsLoading(true);
 
     if (!handleRequiredField()) return;
 
@@ -56,7 +58,9 @@ function App() {
       toast.success("Berhasil tambah soal baru!");
     } catch (error) {
       console.error("Error inserting data:", error);
+      setIsLoading(true);
     } finally {
+      setIsLoading(false);
       await fetchData();
       scrollToTop();
     }
@@ -211,9 +215,12 @@ function App() {
               />
               <button
                 type="submit"
-                className="inline-flex items-center justify-center w-full py-2.5 mt-5 text-lg font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800"
+                disabled={isLoading}
+                className={`inline-flex items-center justify-center w-full py-2.5 mt-5 text-lg font-medium text-center text-white ${
+                  isLoading ? "bg-gray-500" : "bg-blue-700 hover:bg-blue-800"
+                } rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900`}
               >
-                Kirim
+                {isLoading ? "Loading...." : "Kirim"}
               </button>
             </div>
           </form>
